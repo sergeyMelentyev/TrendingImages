d607bd50bb17bb8387a26ed2e9d68e4fe5a14816
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-03 21:34:41 +0300

    new  version

---
+++ app/src/main/java/com/example/melentyev/sergey/trendingimages/Photo.java
@@ -0,0 +1,55 @@
+package com.example.melentyev.sergey.trendingimages;
+
+class Photo {
+    private String mTitle;
+    private String mAuthor;
+    private String mAuthorId;
+    private String mLink;
+    private String mTags;
+    private String mImage;
+
+    public Photo(String mTitle, String mAuthor, String mAuthorId, String mLink, String mTags, String mImage) {
+        this.mTitle = mTitle;
+        this.mAuthor = mAuthor;
+        this.mAuthorId = mAuthorId;
+        this.mLink = mLink;
+        this.mTags = mTags;
+        this.mImage = mImage;
+    }
+
+    String getmTitle() {
+        return mTitle;
+    }
+
+    String getmAuthor() {
+        return mAuthor;
+    }
+
+    String getmAuthorId() {
+        return mAuthorId;
+    }
+
+    String getmLink() {
+        return mLink;
+    }
+
+    String getmTags() {
+        return mTags;
+    }
+
+    String getmImage() {
+        return mImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mAuthorId='" + mAuthorId + '\'' +
+                ", mLink='" + mLink + '\'' +
+                ", mTags='" + mTags + '\'' +
+                ", mImage='" + mImage + '\'' +
+                '}';
+    }
+}
