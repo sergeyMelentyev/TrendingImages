7a366f8bc5fec5c9cd869a5c8bef0330dd3a9991
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-03 17:06:29 +0300

    new version

--- app/src/main/java/com/example/melentyev/sergey/trendingimages/GetRawData.java
+++ app/src/main/java/com/example/melentyev/sergey/trendingimages/GetRawData.java
@@ -2,6 +2,8 @@ package com.example.melentyev.sergey.trendingimages;
 
 import android.os.AsyncTask;
 
-class GetRawData extends AsyncTask<String, Void, String>{
+enum DownloadStatus { IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK }
+
+class GetRawData extends AsyncTask<String, Void, String> {
     
 }
